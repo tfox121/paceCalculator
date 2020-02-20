d1ffa487aeeaa9df73a6857d18bb55af30fa3f73
Tom Fox <tfox2@hotmail.co.uk>
2020-02-20 20:55:27 +0000

    Updates

--- precache-manifest.72ab10d21c2d1a9a1791c852cf418622.js
+++ precache-manifest.72ab10d21c2d1a9a1791c852cf418622.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0dd0d7ca4606d7c6fe62a437c46369fc",
+    "revision": "8c51e102719698f6445652e96b5e3109",
     "url": "/paceCalculator/index.html"
   },
   {
-    "revision": "4193b374d1896a1157ba",
-    "url": "/paceCalculator/static/css/main.bd89d1a2.chunk.css"
+    "revision": "c91e68763fb94c7521f2",
+    "url": "/paceCalculator/static/css/main.c62864c4.chunk.css"
   },
   {
     "revision": "48f2161378d407dc03a7",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/paceCalculator/static/js/2.2e74cfe9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4193b374d1896a1157ba",
+    "revision": "c91e68763fb94c7521f2",
     "url": "/paceCalculator/static/js/main.ae638385.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/paceCalculator/precache-manifest.a2c71bd303536c9351ce037d61a339b3.js"
+  "/paceCalculator/precache-manifest.72ab10d21c2d1a9a1791c852cf418622.js"
 );
 
 self.addEventListener('message', (event) => {
